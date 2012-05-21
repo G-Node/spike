5b456c59778633624951e5ade18a960375e5768c
Philipp Meier <pmeier82@googlemail.com>
2012-05-21 19:24:05 +0200

    include/SpikEval

--- celeryconfig.py
+++ celeryconfig.py
@@ -6,11 +6,11 @@ djcelery.setup_loader()
 # celery task broker settings
 BROKER_HOST = "localhost"
 BROKER_PORT = 5672
-BROKER_USER = "portal_user"
+BROKER_USER = "portal"
 BROKER_PASSWORD = "pass"
-BROKER_VHOST = "gnodeportal"
+BROKER_VHOST = "localhost"
 
 # celery task broker result storage settings.
 CELERY_RESULT_BACKEND = "database"
-CELERY_RESULT_DBURI = "mysql://portal_user:pass@localhost/g-node-spike"
+CELERY_RESULT_DBURI = "mysql://portal:pass@localhost:33306/g-node-spike"
 
