a7514750c7883692f796a9c05424bc11a6a4919d
Philipp Meier <pmeier82@googlemail.com>
2012-05-23 15:47:44 +0200

    settings structure

--- django_access.py
+++ django_access.py
@@ -4,18 +4,17 @@ import sys
 # just import this module and you may play with G-Node Django classes
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 #SYSTEM_ROOT = os.path.normpath(os.path.join(os.getcwd(), ".."))
-PROJECT_NAME = PROJECT_PATH[PROJECT_PATH.rfind("/")+1:] # just in case
+PROJECT_NAME = PROJECT_PATH[PROJECT_PATH.rfind('/') + 1:] # just in case
 
 # some path settings - order matters
-to_pythonpath = ( 
+to_pythonpath = (
     './',
     PROJECT_PATH,
     os.path.join(PROJECT_PATH, 'apps/'),
     os.path.join(PROJECT_PATH, 'apps/ext/'),
-    os.path.join(PROJECT_PATH, 'apps/spike_evaluation/'),
     os.path.join(PROJECT_PATH, 'apps/ext/pinax/'),
     os.path.join(PROJECT_PATH, 'apps/ext/pinax/apps/'),
-)
+    )
 
 for path in to_pythonpath:
     #path = os.path.abspath(os.path.join(SYSTEM_ROOT, p))
