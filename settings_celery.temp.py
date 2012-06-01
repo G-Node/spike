b8a040e669eca90769c202b90a57ec1717810554
Philipp Meier <pmeier82@googlemail.com>
2012-06-01 16:57:55 +0200

    file path for eval results

--- settings_celery.temp.py
+++ settings_celery.temp.py
@@ -7,8 +7,8 @@ try:
     djcelery.setup_loader()
 
     BROKER_URL = "amqp://guest@localhost:5672//"
-    #CELERY_RESULT_BACKEND = 'amqp'
-    CELERY_RESULT_BACKEND = 'database'
-    CELERY_RESULT_DBURI = 'mysql://db-user:db-user-pass@localhost/db-name'
+    #CELERY_RESULT_BACKEND = "amqp"
+    CELERY_RESULT_BACKEND = "database"
+    CELERY_RESULT_DBURI = "mysql://db-user:db-user-pass@localhost/db-name"
 except ImportError:
     pass
